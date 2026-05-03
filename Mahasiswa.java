d93b6c4aab5998cdccbc5e1a40c13e0ceed3b019
Rendra Adnan Farid <rendrafarid22@gmail.com>
2026-05-04 00:03:07 +0700

    Update Mahasiswa.java

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -6,7 +6,7 @@
 /**
  *
  * @ketua Rendra Adnan Farid (25104410036)
- * @anggota M. Rizki Desca Firmansyah (25104410041), Ema Netasari(25104410052), Gladis Ayu Diapitaloka(25104410073)
+ * @anggota M. Rizki Desca Firmansyah (25104410041), Ema Netasari (25104410052), Gladis Ayu Diapitaloka(25104410073)
  * Kelas = Informatika B
  * 
  */
